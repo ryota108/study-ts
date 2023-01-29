ca4c87024136f306f9f4511563e1f4707f1e38bd
Ryota <aya.ryo.718@outlook.jp>
2023-01-29 19:13:13 +0900

    lesson 23

--- app.ts
+++ app.ts
@@ -1,16 +1,30 @@
-function combine(input1:number|string,input2:number | string) {
-   let result
-    if(typeof input1 === "number" && typeof input2 === "number"){
-       result = input1 + input2;
-    } else  {
-        result = input1.toString() + input2.toString()
-    }
-    return result;
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number"| "as-text"
+) {
+  let result;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  if (resultConversion === "as-number") {
+    return +result;
+  } else {
+    return result.toString();
+  }
+  return result;
 }
 
-const combinedAges = combine(30,26);
-console.log(combinedAges)
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
 
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
 
-const combinedNames = combine("Max","Anna")
-console.log(combinedNames)
+const combinedName = combine("Ryota", "Max", "as-text");
+console.log(combinedName);
